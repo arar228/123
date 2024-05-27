ea4791a1964542cc8640cf37617997e3171bbe6a
arar228 <148712960+arar228@users.noreply.github.com>
2024-05-27 15:10:51 +0300

    Add files via upload

---
+++ Test 1.py
@@ -0,0 +1,19 @@
+
+def print_hi(name):
+
+    print(f'Hi, {name}') 
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+a = 'hello'
+a = [1, 1.0, 'hello']
+a = (1, 1.0, 'hello')
+a = {1, 1.0, 'hello'}
+a = frozenset({1, 1.0, 'hello'})
+a = {'a':1, 'b': 2}
+a = b'hello'
+a = bytearray(b'hello')
