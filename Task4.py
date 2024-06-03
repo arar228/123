59c760613ce50acadedd40849c18d75327151a4f
arar228 <148712960+arar228@users.noreply.github.com>
2024-06-04 00:44:38 +0300

    Task4.py

--- Task4.py
+++ Task4.py
@@ -1,29 +1,29 @@
 
-empty_tuple = () # Пустой кортеж
-tuple_with_one_item = (1,) # кортеж из одного элемента
+empty_tuple = () 
+tuple_with_one_item = (1,)
 tuple_ = (1, 2, "str")
 print("Содержимое переменной empty_tuple:", empty_tuple,
 type(empty_tuple))
 print("Содержимое переменной tuple_with_one_item:",
 tuple_with_one_item, type(tuple_with_one_item))
 print("Содержимое переменной tuple_:", tuple_, type(tuple_))
-list_ = [] # Пустой список
+list_ = [] 
 chars_list = ["a", "b", "c"]
 print("Содержимое переменной list_:", list_, type(list_))
 print("Содержимое переменной chars_list:", chars_list,
 type(chars_list))
-empty_string = "" # Пустая строка
-str_ = "test" # Строковый тип данных
+empty_string = "" 
+str_ = "test" 
 print("Содержимое переменной empty_string:", empty_string,
 type(empty_string))
 print("Содержимое переменной str_:", str_, type(str_))
-empty_set = set() # Пустое множество
-set_ = {3, 2, 1, 1} # Множество содержит в себе только уникальные элементы
+empty_set = set() 
+set_ = {3, 2, 1, 1} 
 print("Содержимое переменной empty_set:", empty_set,
 type(empty_set))
 print("Содержимое переменной set_:", set_, type(set_))
-empty_dict = {} # Пустой словарь
-dict_ = { # Словарь хранит пары ключ-значение. Ключи должны быть уникальными значениями
+empty_dict = {} 
+dict_ = {}
  "Имя": "Ваше имя",
  "Фамилия": "Ваша фамилия",
  "Возраст": 18,
